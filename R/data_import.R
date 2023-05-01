9feb0e47c72e29703a8c11db840eae7e2750ed53
agreco <greco031@umn.edu>
2023-05-01 15:43:57 -0500

    additional comments added

--- R/data_import.R
+++ R/data_import.R
@@ -8,11 +8,12 @@ dataset_tbl <- read_delim(file = "../data/dataset.csv", delim = "+") %>%
 reviews_tbl <- read_delim(file = "../data/satisfaction_reviews.csv", delim = ".", col_names = FALSE) %>% 
   rename(employee_id = X3,
          q_good = X1,
-         q_bad = X2) # Easier names for later. Spot check: questions are correctly aligned. NAs in dataset are listed "NA" in the file. employee_id created for a join. Company is Google, people doing all kinds of weird punctuation and such to start their comments
+         q_bad = X2) # Easier names for use later. Spot check: questions are correctly aligned. NAs in dataset are listed "NA" in the file. employee_id created for a join. Company is Google, people doing all kinds of weird punctuation and such to start their comments
 
-final_tbl <- full_join(x = dataset_tbl, y = reviews_tbl, by = join_by(employee_id)) %>% # keeping everything, joining by the employee id column. Spot check: Case 1, 4, 1130, and 1470 are correct. Correct order maintained, maintaining all 1470 cases. 
-  relocate(employee_id, .before = Age)
+final_tbl <- full_join(x = dataset_tbl, y = reviews_tbl, by = join_by(employee_id)) %>% # keeping everything, joining by the employee id column. Spot check: Case 1, 4 (missing 1 comment), 28 (missing 2 comments), 1130, and 1470 are correct. Correct order maintained, all 1470 cases present
+  relocate(employee_id, .before = Age) # moved column to the beginning 
 
+# Data Export
 # saveRDS(final_tbl, file = "../data/dataset.rds") # saved to data to prevent manual copy paste for first save, commented out because no longer needed
 saveRDS(final_tbl, file = "../out/dataset.rds") # saved to out after the first time to prevent overwrite
 
