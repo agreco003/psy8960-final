ace0a4a933260d3ee7841d5ca802d8e005041522
agreco <greco031@umn.edu>
2023-05-04 16:37:30 -0500

    Analyses complete and tables built, need visualizations and to review explanations

--- R/statistical.R
+++ R/statistical.R
@@ -2,12 +2,77 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(rstatix)
+library(ggplot2)
 
 # Data Import and Cleaning
-statisical_tbl <- readRDS(file = "../data/fulldataset.rds")
+statisical_tbl <- readRDS(file = "../data/fulldataset.rds") %>%
+  mutate(across(.cols = c(Attrition, Department, BusinessTravel, Gender, EducationField, JobRole, MaritalStatus, Over18, OverTime), .fns = ~ factor(.)))
 
 # Analysis
+## H1
+h1_cor <- cor_test(data = statisical_tbl, vars = c(MonthlyRate, PerformanceRating), use = "everything")
+h1_cor
+
+## H2
+h2_anova <- anova_test(data = statisical_tbl, formula = MonthlyRate ~ Department, dv = MonthlyRate, wid = employee_id, detailed = TRUE)
+h2_anova
 
 
+## H3 - Base functions allowed
+h3_lm <- lm(YearsAtCompany ~ RelationshipSatisfaction*Gender, data = statisical_tbl)
+h3_lm_result <- summary(h3_lm)
+h3_lm_result
+
+# Visualization
+## Visualization for H1
+## Visualization for H2
+## Visualization for H3
 
 # Publication
+### H1 Table
+h1_tbl <- tibble(h1_cor) %>%
+  mutate(across(c(cor:conf.high), ~ str_remove(format(round(., 2), nsmall = 2), "0"))) %>%
+  select(-method)
+h1_tbl
+write_csv(h1_tbl, file = "../out/H1.csv")
+## H1 Interpretation
+paste0("The correlation for H1 was r(", nrow(statisical_tbl),") = ", h1_tbl$cor,", p-value = ",h1_tbl$p,". Therefore, H1 was ",ifelse(h1_cor$p > .05, "not ", ""),"supported.")
+
+## H2 Table
+h2_tbl <- tibble(Component = h2_anova$Effect, SSn = h2_anova$SSn, SSd = h2_anova$SSd, DFn = h2_anova$DFn, DFd = h2_anova$DFd, F = h2_anova$F, p = h2_anova$p) %>%
+  mutate(across(c(F:p), ~ str_remove(format(round(., 2), nsmall = 2), "^0")))
+h2_tbl
+write_csv(h2_tbl, file = "../out/H2.csv")
+## H2 Interpretation
+paste0("The F-statisic for H2 was F(", h2_tbl$DFn,", ",h2_tbl$DFn,") = ",h2_tbl$F,", p-value = ",h2_tbl$p,". Therefore, H2 was ",ifelse(h2_anova$p > .05, "not ", ""),"supported.")
+
+## H3 Table
+h3_tbl <- tibble('Coefficient' = c("Intercept", "Relationship Satisfaction", "Gender", "Interaction:Gender*Rel. Sat."), "Estimate" = h3_lm_result$coefficients[,"Estimate"],'Std Error' = h3_lm_result$coefficients[,"Std. Error"], 't-value' = h3_lm_result$coefficients[,"t value"], 'p' = h3_lm_result$coefficients[,"Pr(>|t|)"]) %>%
+  mutate(across(c(2:5), ~ str_remove(format(round(., 2), nsmall = 2), "0")))
+h3_tbl
+write_csv(h3_tbl, file = "../out/H3.csv")
+
+## H3 Interpretation
+sentence <- function (name_in_quotes, h3_tibblerow){
+  words <- paste0("The ", name_in_quotes,
+  " for model H3 was ",
+  ifelse(h3_lm_result$coefficients[[h3_tibblerow,"Pr(>|t|)"]] > .05, "not ", ""),
+  "significant, as t(",nrow(statisical_tbl),
+  ") = ", 
+  h3_tbl[[h3_tibblerow,3]],
+  ", p-value = ", 
+  h3_tbl$p[[h3_tibblerow]],".")
+  print(words)
+} #replaced commented code below, built to prevent repeat code. 
+sentence("intercept", 1)
+sentence("coefficient for Relationship Satisfaction", 2)
+sentence("coefficient for Gender", 3)
+sentence("interaction Satisfaction and Gender", 4) 
+
+# paste0("The Intercept for model H3 was ",ifelse(h3_lm_result$coefficients[[1,"Pr(>|t|)"]] > .05, "not ", ""),"significant, as t(",nrow(statisical_tbl),") = ", h3_tbl[[1,3]],", p-value = ", h3_tbl$p[[1]],".")
+# 
+# paste0("The coefficient for Relationship Satisfaction in model H3 was ",ifelse(h3_lm_result$coefficients[[2,"Pr(>|t|)"]] > .05, "not ", ""),"significant, as t(",nrow(statisical_tbl),") = ", h3_tbl[[2,3]],", p-value = ", h3_tbl$p[[2]],".")
+# 
+# paste0("The coefficient for Gender in model H3 was ",ifelse(h3_lm_result$coefficients[[3,"Pr(>|t|)"]] > .05, "not ", ""),"significant, as t(",nrow(statisical_tbl),") = ", h3_tbl[[3,3]],", p-value = ", h3_tbl$p[[3]],".")
+# 
+# paste0("The coefficient for the interaction between Relationship Satisfaction and Gender in model H3 was ",ifelse(h3_lm_result$coefficients[[4,"Pr(>|t|)"]] > .05, "not ", ""),"significant, as t(",nrow(statisical_tbl),") = ", h3_tbl[[4,3]],", p-value = ", h3_tbl$p[[4]],".")
