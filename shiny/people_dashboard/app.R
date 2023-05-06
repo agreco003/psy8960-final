b5671af8e5306a115004064b84adc93f4433e373
agreco <greco031@umn.edu>
2023-05-06 11:14:04 -0500

    Built shiny app

--- shiny/people_dashboard/app.R
+++ shiny/people_dashboard/app.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(shiny)
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+# setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # commented out once app was complete
 
 ui <- fluidPage(
   
@@ -12,9 +12,9 @@ ui <- fluidPage(
     sidebarPanel(
       # Drop down selectors
       selectInput("variableSelect",
-                  "Variable of Interest?",
-                  c("Monthly Income", "Attrition", "Job Satisfaction"),
-                  selected = "Monthly Income"),
+                  "Outcome Variable of Interest?",
+                  c("MonthlyIncome", "Attrition", "JobSatisfaction"),
+                  selected = "MonthlyIncome"),
       selectInput("departmentSelect",
                   "Filter by Department?",
                   c("Human Resources", "Research & Development", "Sales", "All"),
@@ -34,50 +34,81 @@ ui <- fluidPage(
     ),
     # Show a plot and a table based on selections
     mainPanel(
-      plotOutput('plot')
-      #tableOutput('table')
-    )
+      plotOutput('plot'), 
+      tableOutput('table')
     )
   )
+)
 server <- function(input, output) {
   skinny_tbl <- readRDS("skinnydataset.rds")
-  output$plot <- renderPlot({
-    skinny_tbl <- skinny_tbl
+   filtered_tbl <- reactive({
+    filtered_skinny_tbl <- skinny_tbl
     # Department Filter
     if (input$departmentSelect != "All") {
-      skinny_tbl <- skinny_tbl %>%
+      filtered_skinny_tbl <- filtered_skinny_tbl %>%
         filter(Department == input$departmentSelect)
     }
     # Education Filter
     if (input$educationSelect != "All") {
-      skinny_tbl <- skinny_tbl %>%
+      filtered_skinny_tbl <- filtered_skinny_tbl %>%
         filter(EducationField == input$educationSelect)
     }
     # Gender Filter
     if (input$genderSelect != "All") {
-      skinny_tbl <- skinny_tbl %>%
+      filtered_skinny_tbl <- filtered_skinny_tbl %>%
         filter(Gender == input$genderSelect)
     }
     # Job Filter
     if (input$jobSelect != "All") {
-      skinny_tbl <- skinny_tbl %>%
-        filter(EducationField == input$jobSelect)
-    }# Plots
-    if (input$variableSelect == "Attrition") {
-      ggplot(skinny_tbl, aes(x = Attrition)) +
-        geom_bar()
-    } 
-    if (input$variableSelect == "Job Satisfaction") {
-      ggplot(skinny_tbl, aes(x = JobSatisfaction)) +
-        geom_bar()
-    } 
-    if (input$variableSelect == "Monthly Income") {
-      ggplot(skinny_tbl, aes(x = MonthlyIncome)) +
-        geom_histogram(binwidth = 1000) + 
+      filtered_skinny_tbl <- filtered_skinny_tbl %>%
+        filter(JobRole == input$jobSelect)
+    }
+    # Outcome Filter
+    if (input$variableSelect != "None") {
+      filtered_skinny_tbl <- filtered_skinny_tbl %>%
+        select(input$variableSelect)
+    }
+  })
+  output$plot <- renderPlot({
+    # Plots
+    if (input$variableSelect == "MonthlyIncome") {
+      filtered_tbl() %>%
+      ggplot(aes(x = MonthlyIncome)) +
+        geom_histogram(binwidth = 1000, color = "white") +
+        labs(x = "Monthly Income", y = "Employee Count") +
         theme_classic()
-    } 
+    }
+    else if (input$variableSelect == "Attrition") {
+      filtered_tbl() %>%
+        ggplot(aes(x = Attrition)) +
+        geom_bar() +
+        theme_classic()
+    }
+    else if (input$variableSelect == "JobSatisfaction") {
+      filtered_tbl() %>%
+        ggplot(aes(x = JobSatisfaction)) +
+        geom_bar() +
+        labs(x = "Job Satisfaction", y = "Employee Count") + 
+        theme_classic()
+    }
+  })
+  output$table <- renderTable({
+    # Tables
+    if (input$variableSelect == "MonthlyIncome") {
+      summarize(filtered_tbl(), Mean = mean(MonthlyIncome),
+                SD = sd(MonthlyIncome))
+    }
+    else if (input$variableSelect == "Attrition") {
+      summarize(filtered_tbl(), Mean = mean(as.numeric(Attrition) - 1),
+                SD = sd(as.numeric(Attrition))
+      )
+    }
+    else if (input$variableSelect == "JobSatisfaction") {
+      summarize(filtered_tbl(), Mean = mean(JobSatisfaction),
+                SD = sd(JobSatisfaction)
+      )
+    }
   })
 } 
-
-# Run the application 
+#Run the application 
 shinyApp(ui = ui, server = server)
